fc0fff98750bdab7ff728b3164081898bc6e5646
masudahokuto <172101440+masudahokuto@users.noreply.github.com>
2024-10-25 23:39:03 +0900

    carrygame[add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'users/index'
   get 'users/new'
+  get 'users/show'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
